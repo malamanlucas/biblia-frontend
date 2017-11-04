4a959d047d2273cce799aca6e4b4459cf73aa8b5
Lucas Malaman <malamanlucas@gmail.com>
2017-11-04 00:29:22 -0200

    a

--- src/store/index.js
+++ src/store/index.js
@@ -11,8 +11,8 @@ const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
   modules: {
-    // bibliaModule,
-    // enumsModule
+    bibliaModule,
+    enumsModule
   },
   strict: debug,
   plugins: [createPersistedState()]
