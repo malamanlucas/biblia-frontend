4f3115b3cc503d89b9af91f77fdf762bd07a318d
Lucas Malaman <malamanlucas@gmail.com>
2017-12-08 21:01:52 -0200

    toggleSelectALL

--- src/components/Sentenca/Sentenca.vue
+++ src/components/Sentenca/Sentenca.vue
@@ -3,40 +3,54 @@
 
     <button type="button" id="copy-button" class="disappear">CopyButton</button>
 
-    <div class="row">
-      <div class="col-xs-26">
-        <div class="input-group">
+    <div class="sentenca__header">
+      <div class="row">
+        <div class="col-xs-26 col-sm-18">
+          <div class="input-group">
 
-          <input type="text" @keyup.enter="buscar" v-validate="'required|min:2'" name="termo"
+            <input type="text" @keyup.enter="buscar" v-validate="'required|min:2'" name="termo"
             class="form-control" :class="{'hasError': validation.has('termo')}"  placeholder="Exemplo: sodoma" v-model="termo" />
 
-          <span class="input-group-btn">
-            <button type="button" class="btn btn-default" @click="buscar">Buscar</button>
-          </span>
+            <span class="input-group-btn">
+              <button type="button" class="btn btn-default" @click="buscar">Buscar</button>
+            </span>
 
+          </div>
+        </div>
+        <div class="col-xs-26 col-sm-8 sentenca__header__right" v-if="sentencas">
+          <button type="button" class="col-xs-9 col-sm-13 btn btn-primary" @click="toggleSelectAll">
+            <template v-if="!isAllSelected">
+              Select ALL
+            </template>
+            <template v-else>
+              UnSelect ALL
+            </template>
+          </button>
+          <div class="col-xs-17 col-sm-13 sentenca__header__right__ocorrencias">
+            {{ sentencas.total }} ocorrência(s)
+          </div>
         </div>
       </div>
     </div>
 
     <loading ref="loading"></loading>
 
-    <div class="row list" v-if="sentencas">
-      <div class="col-xs-26 list__title text-info h4 text-center">
-        {{ sentencas.total }} ocorrência(s)
-      </div>
-      <div class="col-xs-26">
-        <ul class="list-group">
-          <li class="list-group-item list__item" v-for="(item, index) in sentencas.textos" @click.self="selectItem(item, index)">
-            <div class="list__item__content" @click.stop.prevent="forceClickParent">
-              <div class="list__item__content__checkbox">
-                <input type="checkbox" :id="`checkbox${index}`" >
+    <template v-if="sentencas">
+      <div class="row sentenca__list">
+        <div class="col-xs-26">
+          <ul class="list-group">
+            <li class="list-group-item sentenca__list__item" v-for="(item, index) in sentencas.textos" @click.self="selectItem(item, index)">
+              <div class="sentenca__list__item__content" @click.stop.prevent="forceClickParent">
+                <div class="sentenca__list__item__content__checkbox">
+                  <input type="checkbox" :id="`checkbox${index}`" >
+                </div>
+                <div class="sentenca__list__item__content__text" v-html="highlight(item)" />
               </div>
-              <div class="list__item__content__text" v-html="highlight(item)" />
-            </div>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </template>
 
   </div>
 </template>
@@ -51,29 +65,44 @@
     data: () => ({
       sentencas: null,
       termo: '',
-      textsToCopy: [''],
+      textsToCopy: [],
       valuesSelected: '',
       copyButton: null
     }),
+    computed: {
+      isAllSelected() {
+        return this.textsToCopy.filter(i => i === 0).length === 0
+      }
+    },
     methods: {
       highlight(item) {
         if (isEmpty(this.termo)) {
           return item
         }
         let regex = new RegExp(`(${this.termo})`, 'gi')
-        return item.replace(regex, '<span class="list__item__content__text__highlight">$1</span>')
+        return item.replace(regex, '<span class="sentenca__list__item__content__text__highlight">$1</span>')
       },
       forceClickParent(event) {
         $(event.srcElement).parents('li').click()
         event.stopPropagation()
       },
+      toggleSelectAll() {
+        if (this.isAllSelected) {
+          $('.sentenca__list__item').removeClass('sentenca__list__item--checked')
+          this.cleanTextsToCopy()
+        } else {
+          this.textsToCopy = this.sentencas.textos
+          $('.sentenca__list__item').addClass('sentenca__list__item--checked')
+          this.copyValues(this.textsToCopy.filter(i => i !== 0))
+        }
+      },
       selectItem(item, index) {
-        this.textsToCopy[index] = this.textsToCopy[index] === 0 ? item : 0
+        this.$set(this.textsToCopy, index, this.textsToCopy[index] === 0 ? item : 0)
 
         if (this.textsToCopy[index] !== 0) {
-          $(event.srcElement).addClass('list__item--checked')
+          $(event.srcElement).addClass('sentenca__list__item--checked')
         } else {
-          $(event.srcElement).removeClass('list__item--checked')
+          $(event.srcElement).removeClass('sentenca__list__item--checked')
         }
 
         this.copyValues(this.textsToCopy.filter(i => i !== 0))
@@ -98,11 +127,14 @@
                 }
               }).then(response => {
                 this.sentencas = response.data
-                this.textsToCopy = Array(this.sentencas.textos.length).fill(0)
+                this.cleanTextsToCopy()
                 this.$refs.loading.hide()
               })
             }
           })
+      },
+      cleanTextsToCopy() {
+        this.textsToCopy = Array(this.sentencas.textos.length).fill(0)
       }
     },
     mounted() {
